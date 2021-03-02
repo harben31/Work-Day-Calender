b2e38e38a4f8d7117c65a5e3d64f3eed442f104b
Benjamin Harris <harben31@gmail.com>
2021-03-02 13:29:18 -0800

    almost there! Colors are working correctly. Local storage render is sorted. Just need the hour to update.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,52 +4,86 @@
 //THEN the past hours are colored gray, present hour green future hours red
 //WHEN the save button icon is clicked
 //THEN the text in the main section is saved in local storage and displayed upon page reload
-//
 
 
+
+// const today = moment();
+let currentHour = moment().format("HH");
+// let currentHour = 11;
 const currentDay = $("#currentDay");
-const hourOfDay = $(".hourOfDay");
-const userInputMtg = $(".userInputMtg");
-const today = moment();
-// const currentHour = today.format("HH");
-const currentHour = 11;
-const saveButton = $(".saveBtn");
 const schedule = $("#schedule");
+const hourOfDay = $(".hourOfDay");
 const eventEl = $(".event")
+const userInputMtg = $(".userInputMtg");
+const saveButton = $(".saveBtn");
 
-const timeArray = []
+const clearBtn = $("#clearBtn");
 
-currentDay.text(today.format("dddd, MMM Do, YYYY"));
+const timeArray = [];
+
+const storageKeyArray = [];
 
 
-// const renderMtg = function(){
-//     if(localStorage.getItem())
+currentDay.text(moment().format("dddd, MMM Do, YYYY, hh:mm:ss"))
 
-// }
-for (i=0; i<userInputMtg.length; i++){
-    console.log(i);
-    console.log(userInputMtg[i].dataset.hour);
-    console.log(localStorage.key(i));
-        if(localStorage.key(i)===null){
-            console.log("break")
-            break
-        } else if(userInputMtg[i].dataset.hour===localStorage.key(i)){
-            const key = localStorage.key(i);
-            const value = localStorage.getItem(key);
-            console.log(key);
-            console.log(value);
-            userInputMtg[i].innerHTML = value;
-            // userInputMtg[i].text.(value);
-        }
-            
-} 
+
+// sortedStorageKeyArray = sortStorageKeyArray()
+var timeChange = function(){
+    // console.log("ticktiock")
+    const timeNow = moment().format("dddd, MMM Do, YYYY, hh:mm:ss");
+    currentDay.text(timeNow);
+    const timeChange = moment().format("HH");
+    currentHour = timeChange;
+    // currentHour = moment().format("HH");
+    // currentHour++;
+    // console.log(currentHour);
+}
+
+
+const renderMtg = function(){
+    
+    for (let x=0; x<localStorage.length; x++){
+        storageKeyArray.push(localStorage.key(x));
+    }
+    const sorted = storageKeyArray.sort(function(a, b){return a-b});
+    console.log(sorted);
+
+    console.log(localStorage);
+    // -------------printing locale storage---------------
+    for (let i=0; i<userInputMtg.length; i++){
+        console.log(i);
+        console.log("inputIndex: " + userInputMtg[i].dataset.hour);
+        console.log("storageIndex: " + localStorage.key(i));
+        console.log(sorted[i]);
+
+            if(localStorage.key(i)===null){
+                console.log("break");
+                break;
+            } else if(userInputMtg[i].dataset.hour===sorted[i]){
+                // const key = localStorage.key(i);
+                const key = sorted[i];
+                const value = localStorage.getItem(key);
+                console.log(key);
+                // console.log(value);
+                userInputMtg[i].innerHTML = value;
+                // userInputMtg[i].text.(value);
+            }    
+    } 
+    // console.log(storageKeyArray);
+    // console.log(sortedStorageKeyArray);
+}
+
+renderMtg();
 
 // console.log(userInputMtg[j].attr("data-hour") <currentHour);
 // console.log(userInputMtg[j].attr("data-hour") ===currentHour);
 // console.log(userInputMtg[j].attr("data-hour") >currentHour);
 
+
+// -------------time of day color change-------------
+//need to make it change on the hour
 for(let j=0; j<userInputMtg.length; j++){
-    console.log(eventEl[j]);
+    // console.log(eventEl[j]);
     if (userInputMtg[j].dataset.hour<currentHour){
         $(eventEl[j]).removeClass("future").addClass("past");
     }  else if(userInputMtg[j].dataset.hour>currentHour){
@@ -57,15 +91,18 @@ for(let j=0; j<userInputMtg.length; j++){
     } else if(userInputMtg[j].dataset.hour===currentHour){
         $(eventEl[j]).removeClass("past").addClass("present");
     }
-    console.log(userInputMtg[j].dataset.hour <currentHour);
-    console.log(userInputMtg[j].dataset.hour ===currentHour);
-    console.log(userInputMtg[j].dataset.hour >currentHour);
+    // console.log(userInputMtg[j].dataset.hour <currentHour);
+    // console.log(userInputMtg[j].dataset.hour ===currentHour);
+    // console.log(userInputMtg[j].dataset.hour >currentHour);
 
-    console.log(userInputMtg[j].dataset.hour)
-    console.log(currentHour)
+    // console.log(userInputMtg[j].dataset.hour)
+    // console.log(currentHour)
 }
 
 
+clearBtn.on("click", function(){
+    localStorage.clear();
+})
 
 
 
@@ -74,3 +111,5 @@ schedule.on("click", "i", function(event){
     localStorage.setItem(event.target.dataset.hour, $(event.target).closest(".hourOfDay").find("textarea").val());
     
 });
+
+setInterval(timeChange, 1000)
