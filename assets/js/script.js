24248da7824f159fcb91ef1a038a41986571d9db
Benjamin Harris <harben31@gmail.com>
2021-03-08 13:43:08 -0800

    polishing code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 
 let currentHour = moment().format("HH");
-// let currentHour = "09";
 const currentDay = $("#currentDay");
 const schedule = $("#schedule");
 const hourOfDay = $(".hourOfDay");
@@ -15,7 +14,6 @@ const storageKeyArray = [];
 currentDay.text(moment().format("dddd, MMM Do, YYYY, hh:mm:ss a"))
 
 for(let j=0; j<userInputMtg.length; j++){
-       // console.log(eventEl[j]);
     let currentHourString = currentHour.toString();
     if (userInputMtg[j].dataset.hour<currentHour){
         $(eventEl[j]).removeClass("future").addClass("past");
@@ -30,26 +28,6 @@ var timeChange = function(){
     //jumbotron time
     const timeNow = moment().format("dddd, MMM Do, YYYY, hh:mm:ss a");
     currentDay.text(timeNow);
-    
-    // currentHour++;
-
-    // //checking the time and changing color. too heavy to loop every second?
-    // //didn't work. scope?
-    // //Not scope. I don't think. I think that it is changing the class but
-    // //the page is not refreshing so the change won't render. I think. 
-    // //nope cuz it works when i increment a fake hour
-    // //when I input the fake hour the current class does not work but the other class do change
-    // //fake hour is still wierd but has to do with type
-    // for(let j=0; j<userInputMtg.length; j++){
-    //     // console.log(eventEl[j]);
-    //     if (userInputMtg[j].dataset.hour<currentHour){
-    //         $(eventEl[j]).removeClass("future").addClass("past");
-    //     } else if (userInputMtg[j].dataset.hour===currentHour){
-    //         $(eventEl[j]).removeClass("past").addClass("present");
-    //     } else if (userInputMtg[j].dataset.hour>currentHour){
-    //         $(eventEl[j]).removeClass("present").addClass("future");
-    //     }; 
-    // };
 }
 
 // -----getting and rendering localestorage----------  
